60b883607ddcd1cb87abf7d52f86828e7644fa4f
Jackie Chen <cheezballz3.14@gmail.com>
2020-12-08 12:28:18 -0500

    Make Player

--- Player.gd
+++ Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 export var speed = 400
- 
+export var jump_speed = 600
+
+var velocity
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,9 +12,33 @@ export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var accel = Vector2()
+	
+	# Run
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		$AnimatedSprite.animation = "run"
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		$AnimatedSprite.animation = "run"
+	else:
+		velocity.x = 0
+		$AnimatedSprite.animation = "idle"
+		
+	# Jump
+	if Input.is_action_pressed("ui_up"):
+		accel.y += jump_speed
+	
+	accel.y -= ProjectSettings.get_setting("physics/2d/default_gravity")
+	
+	velocity += accel * delta
+	velocity = move_and_slide(velocity)
+	
+func _process(delta):
+	position += velocity * delta
+	
+	
