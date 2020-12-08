cb129cf8bc03424d33e1a88102451e52b5a82217
Jackie Chen <cheezballz3.14@gmail.com>
2020-12-08 12:49:05 -0500

    Finish Player

--- Player.gd
+++ Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
-export var speed = 400
-export var jump_speed = 600
+export var speed = 80
+export var jump_speed = 100
 
+var move_state
+var jump_state
 var velocity
 
 # Declare member variables here. Examples:
@@ -12,6 +14,8 @@ var velocity
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	move_state = "idle"
+	jump_state = "grounded"
 	velocity = Vector2()
 
 
@@ -21,24 +25,38 @@ func _physics_process(delta):
 	# Run
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
-		$AnimatedSprite.animation = "run"
+		move_state = "run"
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
-		$AnimatedSprite.animation = "run"
+		move_state = "run"
 	else:
+		move_state = "idle"
 		velocity.x = 0
-		$AnimatedSprite.animation = "idle"
 		
 	# Jump
-	if Input.is_action_pressed("ui_up"):
-		accel.y += jump_speed
+	if Input.is_action_pressed("ui_up") and jump_state == "grounded":
+		jump_state = "airborne"
+		velocity.y -= jump_speed
 	
-	accel.y -= ProjectSettings.get_setting("physics/2d/default_gravity")
+	accel.y += ProjectSettings.get_setting("physics/2d/default_gravity")
 	
 	velocity += accel * delta
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+		
+	match(is_on_floor()):
+		true: jump_state = "grounded"
+		false: jump_state = "airborne"
+		
+	print(move_state + " " + jump_state)
 	
 func _process(delta):
+	match(move_state):
+		"run":
+			$AnimatedSprite.animation = "run"
+			$AnimatedSprite.flip_h = (velocity.x <= 0)
+		"idle":
+			$AnimatedSprite.animation = "idle"
+
 	position += velocity * delta
 	
 	
